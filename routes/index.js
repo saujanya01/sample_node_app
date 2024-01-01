cf327ad8baef842893585458a74787134c6e88c7
Saujan ya Tiwari <saujanyatiwari455@gmail.com>
2024-01-01 13:06:39 +0530

    nvim

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+    backend_url: process.env.REACT_APP_RMF_SERVER,
+  });
+});
+
+router.get("/hello-from-nvim", (req, res)=>{
+    res.status(200).json({message: "this route was written in nvim"})
+}
+)
+
+router.get("/test", function (req, res, next) {
+  res.status(200).json({ message: "Hello World!" });
 });
 
 module.exports = router;
